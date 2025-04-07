2ece4e313998f1b35b8837f111b2a899ffcc01c2
Kaium Al Limon <limon.3cs@gmail.com>
2025-04-08 01:48:07 +0600

    feat: add logout route to authentication module

--- src/modules/auth/routes/auth.route.js
+++ src/modules/auth/routes/auth.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, testProtected } = require("../controllers/auth.controller");
+const { registerUser, loginUser, logoutUser, testProtected } = require("../controllers/auth.controller");
 const { verifyToken } = require("../../../middlewares/auth.middleware");
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/verify", verifyToken, testProtected);
+router.get("/logout", logoutUser); 
 
 module.exports = router;
